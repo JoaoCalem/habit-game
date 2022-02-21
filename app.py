b95ba6786138f26ffdd7f21427b7057b0c8cfa46
Joao Calem <joaocalem@gmail.com>
2022-02-21 17:37:31 +0000

    added sql interactions

--- app.py
+++ app.py
@@ -2,33 +2,57 @@ import streamlit as st
 import json
 import datetime as dt
 import pandas as pd
+import psycopg2
+
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def sql_connection():
+    conn = psycopg2.connect(
+    host="ec2-54-83-21-198.compute-1.amazonaws.com",
+    database="d5nu5a60baen6j",
+    user="yucdrxbhlqsiku",
+    password="2d63af339af82fc84dfa28985910c1fd2b423d7dd3618a6cc24926928d9e6571")
+
+    return conn
 
 def save():
-    json.dump(points,open("points.json","w"))
-    json.dump(habits,open("habits.json","w"))
-    json.dump(rewards,open("rewards.json","w"))
+    #json.dump(points,open("points.json","w"))
+    #json.dump(habits,open("habits.json","w"))
+    #json.dump(rewards,open("rewards.json","w"))
+    conn.commit()
     st.experimental_rerun()
     
+conn = sql_connection()
 
+cur = conn.cursor()
+    
 weekly_points = 70
     
-points = json.load(open("points.json"))
+cur.execute("SELECT * FROM points")
+points = cur.fetchone()[1]
 
-habits = json.load(open("habits.json"))
+cur.execute("SELECT * FROM habits")
+habits = {r[0]: {"name": r[1], "goal":r[2], "type":r[3]} for r in cur.fetchall()}
 
-rewards = json.load(open("rewards.json"))
+cur.execute("SELECT * FROM rewards")
+rewards = {r[0]: {"name": r[1],"points":r[2], "type":r[3]} for r in cur.fetchall()}
+
+cur.execute("SELECT * FROM counts")
+counts = cur.fetchall()
 
 def trash():
     if st.button('Reset'):
         # print is visible in the server output, not in the page
-        json.dump({"total":0},open("points.json","w"))
-        json.dump({},open("habits.json","w"))
-        json.dump({},open("rewards.json","w"))
+        #json.dump({"total":0},open("points.json","w"))
+        #json.dump({},open("habits.json","w"))
+        #json.dump({},open("rewards.json","w"))
+
+        conn.rollback()
+        conn.close()
         st.experimental_rerun()
 
 st.write("## Points:")
 
-st.write(f'### {points["total"]}')
+st.write(f'### {points}')
 
 if habits:
     st.write("## Habits:")
@@ -38,7 +62,10 @@ for key,habit in habits_st.items():
 
     start = dt.date.today() - dt.timedelta(days=dt.date.today().weekday())
     for day in range(7):
-        habits[key]["counts"][str(start + dt.timedelta(day))] = habits[key]["counts"].get(str(start + dt.timedelta(day)),0)
+        day_datetime = start + dt.timedelta(day)
+        cur.execute(f"SELECT * FROM counts WHERE habit_id = {key} AND date = '{day_datetime}'")
+        if not cur.fetchone():
+            cur.execute(f"INSERT INTO counts (habit_id, date, count) VALUES ({key}, '{day_datetime}', 0)")
 
     if habits[key]["type"] == "Time Based":
         count_type = "hours "
@@ -52,22 +79,41 @@ for key,habit in habits_st.items():
         habit["main"][2].write("# ")
         if habit["main"][2].button('Add Time',key=f'{habits[key]["name"]} add'):
             # print is visible in the server output, not in the page
-            habits[key]["counts"][str(dt.date.today())] += int(mins) + 60*int(hours)
-            points["total"] += weekly_points*(int(mins) + 60*int(hours))/(len(habits)*60*float(habits[key]["goal"]))
+            cur.execute(f"SELECT count FROM counts WHERE habit_id = {key} AND date = '{dt.date.today()}'")
+            temp = float(cur.fetchone()[0]) + int(mins) + 60*int(hours)
+            cur.execute(f"""UPDATE counts SET count={temp}
+                WHERE habit_id = {key} AND date = '{dt.date.today()}'""")
+            
+            cur.execute(f"SELECT total FROM points")
+            temp = float(cur.fetchone()[0]) + weekly_points*(int(mins) + 60*int(hours))\
+                /(len(habits)*60*float(habits[key]["goal"]))
+            cur.execute(f"UPDATE points SET total={temp}")
+            
             save()
-
-        count = sum(habits[key]["counts"].values())/60
+        cur.execute(f"SELECT c.date, c.count/60 FROM counts c WHERE date >= '{start}' AND habit_id={key}")
 
     else:
         count_type = ""
 
         if habit["expander"].button('+',key=f'{habits[key]["name"]} add'):
-            habits[key]["counts"][str(dt.date.today())] += 1
-            points["total"] += weekly_points/(len(habits)*float(habits[key]["goal"]))
+            cur.execute(f"SELECT count FROM counts WHERE habit_id = {key} AND date = '{dt.date.today()}'")
+            temp = float(cur.fetchone()[0]) + 1
+            cur.execute(f"""UPDATE counts SET count={temp}
+                WHERE habit_id = {key} AND date = '{dt.date.today()}'""")
+            
+            cur.execute(f"SELECT total FROM points")
+            temp = float(cur.fetchone()[0]) + weekly_points/(len(habits)*float(habits[key]["goal"]))
+            cur.execute(f"UPDATE points SET total={temp}")
+            
             save()
-        count = sum(habits[key]["counts"].values())
+            
+        cur.execute(f"SELECT c.date, c.count FROM counts c WHERE date >= '{start}' AND habit_id={key}")
+    
+    temp_counts = {str(i[0]):float(i[1]) for i in cur.fetchall()}
+    
+    count = sum(temp_counts.values())
 
-    habit["expander"].bar_chart(pd.DataFrame(habits[key]["counts"].values(),index=habits[key]["counts"].keys()))
+    habit["expander"].bar_chart(pd.DataFrame(temp_counts.values(),index=temp_counts.keys()))
 
     habit["expander"].write(f'### {count} out of {habits[key]["goal"]} {count_type}complete this week')
 
@@ -75,8 +121,10 @@ for key,habit in habits_st.items():
     habit["end_columns"] = habit["expander"].columns([4,1])
 
     if habit["end_columns"][1].button('Delete Habit',f'{habits[key]["name"]} delete'):
-        del habits[key]
+        cur.execute(f"DELETE FROM counts WHERE habit_id={key}")
+        cur.execute(f"DELETE FROM habits WHERE id={key}")
         save()
+    conn.commit()
 
 if rewards:
     st.write("## Rewards:")
@@ -84,12 +132,13 @@ if rewards:
 reward_sts = {key:{} for key in rewards.keys()}
 for key,reward in rewards.items():
     reward_st = reward_sts[key]
+    reward["points"] = float(reward["points"])
     
     if reward["type"] == "Count Based":
         reward_st["columns"] = st.columns([3.5,0.5,1])
         reward_st["columns"][0].write(f"##### ")
         reward_st["columns"][0].write(f"##### ")
-        reward_st["columns"][0].write(f"##### {key}")
+        reward_st["columns"][0].write(f"##### {reward['name']}")
         reward_st["columns"][0].write(f"##### ")
         reward_st["columns"][0].write(f"##### ")
         reward_st["columns"][1].write(f"##### ")
@@ -99,9 +148,12 @@ for key,reward in rewards.items():
         reward_st["columns"][1].write(f"##### ")
         reward_st["columns"][2].write(f"##### ")
         reward_st["columns"][2].write(f"##### ")
-        if reward["points"] <= points["total"]:
+        if reward["points"] <= points:
             if reward_st["columns"][2].button("Use", key=f'{key} use'):
-                points["total"] += -reward["points"]
+                cur.execute(f"SELECT total FROM points")
+                temp = float(cur.fetchone()[0]) - reward["points"]
+                cur.execute(f"UPDATE points SET total={temp}")
+                
                 save()
         else:
             reward_st["columns"][2].write('Not enough')
@@ -111,7 +163,7 @@ for key,reward in rewards.items():
         reward_st["columns"] = st.columns([1.2,1.1,1.1,0.5,1])
         reward_st["columns"][0].write(f"##### ")
         reward_st["columns"][0].write(f"##### ")
-        reward_st["columns"][0].write(f"##### {key}")
+        reward_st["columns"][0].write(f"##### {reward['name']}")
         reward_st["columns"][0].write(f"##### ")
         reward_st["columns"][0].write(f"##### ")
         reward_st["hours"] = reward_st["columns"][1].number_input('Hours', 0,key=f'{key} reward hours')
@@ -124,9 +176,11 @@ for key,reward in rewards.items():
         reward_st["columns"][3].write(f"##### ")
         reward_st["columns"][4].write(f"##### ")
         reward_st["columns"][4].write(f"##### ")
-        if cost <= points["total"]:
+        if cost <= points:
             if reward_st["columns"][4].button("Use", key=f'{key} use'):
-                points["total"] += -cost
+                cur.execute(f"SELECT total FROM points")
+                temp = float(cur.fetchone()[0]) - cost
+                cur.execute(f"UPDATE points SET total={temp}")
                 save()
         else:
             reward_st["columns"][4].write('Not enough')
@@ -147,7 +201,7 @@ new_habit[3].write("# ")
 if new_habit[3].button('Add Habbit'):
     # print is visible in the server output, not in the page
     if name and goal:
-        habits[name] = {"name":name, "goal":goal, "type":option, "counts":{}}
+        cur.execute(f"INSERT INTO habits (name,goal,type) VALUES ('{name}','{goal}','{option}')")
         save()
     else:
         pass
@@ -167,7 +221,8 @@ new_reward[3].write("# ")
 if new_reward[3].button('Add Reward'):
     # print is visible in the server output, not in the page
     if reward_name and reward_points:
-        rewards[reward_name] = {"points":reward_points, "type":reward_option}
+        cur.execute(f"""INSERT INTO rewards (name,points,type)
+            VALUES ('{reward_name}','{reward_points}','{reward_option}')""")
         save()
     else:
         pass
